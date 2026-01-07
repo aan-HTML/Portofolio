e0a7a950c91678771ea55c7c3226c5155e98c1aa
Aann <161026795+aan-HTML@users.noreply.github.com>
2026-01-07 21:37:12 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -81,35 +81,24 @@
 	    $('#ftco-nav').collapse('hide');
 	    $('.js-fh5co-nav-toggle').removeClass('active');
 		});
-
+$(document).ready(function () {
+  $('.home-slider').owlCarousel({
+    loop: true,
+    autoplay: true,
+    margin: 0,
+    animateOut: 'fadeOut',
+    animateIn: 'fadeIn',
+    nav: false,
+    items: 1
+  });
+});
 	};
 
 	onePageClick();
 	
 
 	var carousel = function() {
-		$('.home-slider').owlCarousel({
-	    loop:true,
-	    autoplay: true,
-	    margin:0,
-	    animateOut: 'fadeOut',
-	    animateIn: 'fadeIn',
-	    nav:false,
-	    autoplayHoverPause: false,
-	    items: 1,
-	    navText : ["<span class='ion-md-arrow-back'></span>","<span class='ion-chevron-right'></span>"],
-	    responsive:{
-	      0:{
-	        items:1
-	      },
-	      600:{
-	        items:1
-	      },
-	      1000:{
-	        items:1
-	      }
-	    }
-		});
+		
 	};
 	carousel();
 
